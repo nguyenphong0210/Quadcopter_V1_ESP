881b237b620d6548597680cca6c9b653d4c82b4b
Nguyen Phong <nguyenphong0210pt@gmail.com>
2023-11-05 17:51:49 +0700

    17h51

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,18 @@
 #include <Arduino.h>
+#include <MPU6050.h>
 #include <Wire.h>
-#include "Signals.h"
+#include <Signals.h>
+
+MPU6050 mpu6050(Wire);
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
   Signal_initialize();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   Signal_step();
+  Serial.println(cmd_inport.live_time_ticks);
 }
