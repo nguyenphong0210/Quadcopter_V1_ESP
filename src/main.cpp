ff61e2fc11740cc4fc817b4bed2bb4def6fcc176
Nguyen Phong <nguyenphong0210pt@gmail.com>
2023-11-09 21:34:28 +0700

    commit 0911

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,24 @@
 #include <Arduino.h>
 #include <Wire.h>
+
 #include <Signals.h>
 #include <Sensors.h>
 #include <Environment.h>
 #include <linearAirframe.h>
 #include <flightControlSystem.h>
 
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(4800);
+
   Signal_initialize();
   Sensors_initialize();
   Environment_initialize();
   flightControlSystem_initialize();
   linearAirframe_initialize();
+
+  delay(3000);
 }
 
 void loop() {
@@ -23,6 +28,7 @@ void loop() {
   Environment_step();
   flightControlSystem_step();
   linearAirframe_step();
+
   Serial.print(motors_outport[0]);
   Serial.print("  ");
   Serial.print(motors_outport[1]);
@@ -30,4 +36,5 @@ void loop() {
   Serial.print(motors_outport[2]);
   Serial.print("  ");
   Serial.println(motors_outport[3]);
+
 }
